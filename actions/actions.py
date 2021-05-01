46732c0a35cfc5499a1426bd58484ef718c32297
Nam Nguyen <namhoai167@gmail.com>
2021-05-01 22:57:21 +0700

    Small changes

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,16 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from fitbert import FitBert
+from transformers import (
+    BlenderbotSmallTokenizer, 
+    BlenderbotSmallForConditionalGeneration,
+    BlenderbotTokenizer,
+    BlenderbotForConditionalGeneration,
+    ElectraForMaskedLM,
+    ElectraTokenizer,
+    pipeline
+)
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -29,12 +39,12 @@ class ActionHelloWorld(Action):
 class ActionOnFallBack(Action):
 
     def name(self) -> Text:
-        return "utter_fallback_warning"
+        return "utter_fallback_chat"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="It's a fallback!")
+        dispatcher.utter_message(text="Here's a fallback! Implement Blenderbot here")
 
         return []
