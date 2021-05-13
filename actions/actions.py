f362666d999a6eb0614f4130160047164ab18974
thanhmannguyen3158 <66194911+thanhmannguyen3158@users.noreply.github.com>
2021-05-13 17:32:30 +0700

    Xóa các intent mẫu

--- actions/actions.py
+++ actions/actions.py
@@ -23,19 +23,6 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
 
-# This is a simple example for a custom action which utters "Hello World!"
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
 
 class ActionOnFallBack(Action):
 
@@ -48,16 +35,15 @@ class ActionOnFallBack(Action):
         # Load BlenderBot
         # Maybe we should put this in init function or __init__.py later on
         BB_PATH = './blenderbot_small-90M'
-        BBModel = BlenderbotSmallForConditionalGeneration.from_pretrained(BB_PATH)
+        BBModel = BlenderbotSmallForConditionalGeneration.from_pretrained(
+            BB_PATH)
         BBTokenizer = BlenderbotSmallTokenizer.from_pretrained(BB_PATH)
-
         latest_user_message = tracker.latest_message['text']
-
         inputs = BBTokenizer([latest_user_message], return_tensors='pt')
         reply_ids = BBModel.generate(**inputs)
-        bot_reply = BBTokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0]
+        bot_reply = BBTokenizer.batch_decode(
+            reply_ids, skip_special_tokens=True)[0]
         dispatcher.utter_message(text=str(bot_reply))
-
         return []
 
 
@@ -100,8 +86,9 @@ class ActionRequestToTracau(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        query = tracker.latest_message['entities'][-1]['value'].strip('"').strip()
-        #dispatcher.utter_message(text=query)
+        query = tracker.latest_message['entities'][-1]['value'].strip(
+            '"').strip()
+        # dispatcher.utter_message(text=query)
         url = "https://api.tracau.vn/WBBcwnwQpV89/s/" + query + "/en"
         try:
             respond = rq.get(url).json()
@@ -114,18 +101,23 @@ class ActionRequestToTracau(Action):
             html = respond['tratu'][0]['fields']['fulltext']
         parsed_html = BeautifulSoup(html)
         if parsed_html.find("article", {'data-tab-name': "Ngữ pháp"}):
-            l = [x for x in parsed_html.find("article", {'data-tab-name': "Ngữ pháp"}).find("div", {'class': "dict--content"}).children]
+            l = [x for x in parsed_html.find("article", {
+                                             'data-tab-name': "Ngữ pháp"}).find("div", {'class': "dict--content"}).children]
         elif parsed_html.find("article", {'data-tab-name': "Thành ngữ"}):
-            l = [x for x in parsed_html.find("article", {'data-tab-name': "Thành ngữ"}).find("div", {'class': "dict--content"}).children]
+            l = [x for x in parsed_html.find("article", {
+                                             'data-tab-name': "Thành ngữ"}).find("div", {'class': "dict--content"}).children]
         elif parsed_html.find("article", {'data-tab-name': "Anh - Anh"}):
-            l = [x for x in parsed_html.find("article", {'data-tab-name': "Anh - Anh"}).find("div", {'class': "dict--content"}).children]
+            l = [x for x in parsed_html.find("article", {
+                                             'data-tab-name': "Anh - Anh"}).find("div", {'class': "dict--content"}).children]
         else:
-            dispatcher.utter_message(text="Sorry, I can't find the phrase {0} in the database".format(query))
+            dispatcher.utter_message(
+                text="Sorry, I can't find the phrase {0} in the database".format(query))
             return []
         l2 = []
         for element in l:
             if element.name == 'dtrn':
-                definition_text = element.find('dtrn',text=True, recursive=False)
+                definition_text = element.find(
+                    'dtrn', text=True, recursive=False)
                 l2.append(definition_text)
                 for child in element.children:
                     if child.string:
@@ -133,5 +125,6 @@ class ActionRequestToTracau(Action):
             else:
                 l2.append(element.getText())
 
-        dispatcher.utter_message(text='\n'.join([string for string in l2 if string]))
+        dispatcher.utter_message(text='\n'.join(
+            [string for string in l2 if string]))
         return []
